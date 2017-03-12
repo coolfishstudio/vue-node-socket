d536f8f6d1d760fad36a5f9d759abb03ed6402f0
coolfish <461836324@qq.com>
2017-03-12 14:38:32 +0800

    聊天室的样式

--- client/components/room.vue
+++ client/components/room.vue
@@ -1,8 +1,13 @@
 <template>
     <div class="room">
-        <h1>{{ msg }}</h1>
-        
-        </ul>
+        <div>头部</div>
+        <div class="content">内容</div>
+        <div class="footer">
+            <div class="main">
+                <input type="text" class="input" />
+                <div class="send">发送</div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -11,29 +16,68 @@ export default {
     name: 'room',
     data () {
         return {
-            msg: 'room'
         }
-    }
+    },
+
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-    font-weight: normal;
-}
-
-ul {
-    list-style-type: none;
-    padding: 0;
-}
-
-li {
-    display: inline-block;
-    margin: 0 10px;
-}
-
-a {
-    color: #42b983;
+<style lang="less" scoped>
+.room {
+    width: 100%;
+    height: 100%;
+    position: relative;
+    .content {
+        position: absolute;
+        top: 50px;
+        bottom: 50px;
+        left: 0;
+        right: 0;
+        padding: 10px;
+        overflow-y: auto;
+        box-sizing: border-box;
+    }
+    .footer {
+        position: fixed;
+        bottom: 0;
+        height: 50px;
+        left: 0;
+        right: 0;
+        background-color: #fff;
+        border-top: 1px solid #dedede;
+        padding: 8px 10px;
+        box-sizing: border-box;
+        .main {
+            width: 100%;
+            height: 100%;
+            overflow: hidden;
+            .input {
+                float: left;
+                width: 78%;
+                height: 33px;
+                outline: none;
+                border: 1px solid #ddd;
+                border-radius: 5px 0 0 5px;
+                color: #444;
+                font-size: 14px;
+                padding: 0 10px;
+                background-color: #efefef;
+                box-sizing: border-box;
+            }
+            .send {
+                float: right;
+                width: 22%;
+                height: 33px;
+                outline: none;
+                background-color: #499eff;
+                border-radius: 0 5px 5px 0;
+                text-align: center;
+                line-height: 33px;
+                color: #fff;
+                box-sizing: border-box;
+                cursor: pointer;
+            }
+        }
+    }
 }
 </style>
